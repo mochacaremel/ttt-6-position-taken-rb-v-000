47aba70ad3b52f33aaf0895347f801204e217c05
Asheley McDonald <asheleymcdonald@gmail.com>
2018-03-26 02:23:35 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,24 @@
-def position_taken? (board, index)
-  board = ["" || " " || nil]
-  index = false 
+def position_taken?(board,position)
+  if board[position] == " "
+    false
+    elsif board[position] == ""
+    false
+    elsif  board[position] == nil
+    false
+    else   board[position] == "X" || "O"
+    true
+
   end
+end
+
+
+
+
+
+# def position_taken? (board, index)
+#   board = ["" || " " || nil]
+#   index = false 
+#   end
 
 
 
