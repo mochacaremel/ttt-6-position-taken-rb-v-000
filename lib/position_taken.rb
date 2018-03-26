348041e9498fdb793e97ca7ec9302ac1b3816614
Asheley McDonald <asheleymcdonald@gmail.com>
2018-03-26 02:12:40 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,4 @@
 def position_taken? (board, index)
-  def board(" ", " ", " ", " ", " ", " ", " ", " ", " ") || def board("", "", "", "", "", "", "", "", "")
+  def board(" ", " ", " ", " ", " ", " ", " ", " ", " ") || board("", "", "", "", "", "", "", "", "")
 end
 end
